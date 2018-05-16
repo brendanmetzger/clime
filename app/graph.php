cdd5b7308c403da027044bef217f059d87c3e9cf
Brendan <brendan.metzger@gmail.com>
2018-05-16 15:38:42 -0500

    removing options

--- app/graph.php
+++ app/graph.php
@@ -11,7 +11,7 @@ function rpn($expression, $stack = [], $op = []) {
   foreach (preg_split("/[^-+*\/^.0-9]+/", trim($expression)) as $token) {
     $stack[] = is_numeric($token) ? $token : $op[$token](...array_slice($stack, -2));
   }
-
+  
   return end($stack);
 }
 
@@ -46,35 +46,23 @@ function createGraph($filename, $key, $start, $trend, $label, $title, $autoscale
     $options[] = 'LINE2:smoothed#0400ff';
   }
 
-  # if wind plot show color coded wind direction
+  // Reverse Polish Notation
+  // A,B,C,IF should be read as if (A) then (B) else (C); n q GE should be read as n >= q ? 1 : 0
   if ($key == 'windspeedmph') {
-    $points = array_keys(CONFIG['chart']['o_color']);
+    $ord = array_keys(CONFIG['chart']['o_color']);
     
     $options[] = sprintf('DEF:wDir=%s:winddir:AVERAGE', CONFIG['database']);
     $options[] = 'VDEF:wMax=dSeries,MAXIMUM';
     $options[] = 'CDEF:wMaxScaled=dSeries,0,*,wMax,+,-0.15,*';
+    
     for ($s = 45, $i=$s/2; $i < 360; $i+=$s) {
-      // Reverse Polish Notation
-      // A,B,C,IF should be read as if (A) then (B) else (C)
-      // n q GE should be read as n > q ? 1 : 0
-      $options[] = sprintf('CDEF:%sdir=wDir,%s,GE,wDir,%s,LT,*,wMaxScaled,0,IF', $points[floor($i/$s)], fmod($i - $s + 360, 360), $i);
+      $options[] = sprintf('CDEF:%sdir=wDir,%s,GE,wDir,%s,LT,*,wMaxScaled,0,IF', $ord[floor($i/$s)], fmod($i - $s + 360, 360), $i);
     }
-    
-    // $options[] = 'CDEF:ndir=wDir,337.5,GE,wDir,22.5,LE,*,wMaxScaled,0,IF';
-    // $options[] = 'CDEF:nedir=wDir,22.5,GT,wDir,67.5,LT,*,wMaxScaled,0,IF';
-    // $options[] = 'CDEF:edir=wDir,67.5,GE,wDir,112.5,LE,*,wMaxScaled,0,IF';
-    // $options[] = 'CDEF:sedir=wDir,112.5,GT,wDir,157.5,LT,*,wMaxScaled,0,IF';
-    // $options[] = 'CDEF:sdir=wDir,157.5,GE,wDir,202.5,LE,*,wMaxScaled,0,IF';
-    // $options[] = 'CDEF:swdir=wDir,202.5,GT,wDir,247.5,LT,*,wMaxScaled,0,IF';
-    // $options[] = 'CDEF:wdir=wDir,247.5,GE,wDir,292.5,LE,*,wMaxScaled,0,IF';
-    // $options[] = 'CDEF:nwdir=wDir,292.5,GT,wDir,337.5,LT,*,wMaxScaled,0,IF';
-    
     
     array_push($options, ...array_map(function($ord, $hex) {
       return sprintf('AREA:%sdir%s:%s', $ord, $hex, strtoupper($ord));
-    }, $points , CONFIG['chart']['o_color']));
-
-   }
+    }, $ord , CONFIG['chart']['o_color']));
+  }
   
   $graphObj->setOptions($options);
   $graphObj->save();
